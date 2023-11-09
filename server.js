b394f26617a9472fb9e67155d4fa421c342fbf2f
sev-valdez <severianovaldez@lithia.com>
2023-11-09 10:15:26 -0500

    Add GraphQL Yoga

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const { buildSchema } = require('graphql');
+const { createYoga } = require('graphql-yoga');
+
+const schema = buildSchema(`
+    type Query {
+        description: String
+        price: Float
+    }
+`);
 
 const app = express();
+
+app.use(
+  '/graphql',
+  createYoga({
+    schema: schema,
+    graphiql: true,
+  })
+);
 
 app.listen(3000, () => {
   console.log(`Running GraphQL server...`);
